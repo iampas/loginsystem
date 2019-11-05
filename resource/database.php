0ea996115bf3f7bf1d5a16a7ee54e36bf6ded576
Prasad Sakharkar <prasad.sakharkar@gmail.com>
2019-11-05 13:09:44 +0530

    database.php edit

--- resource/database.php
+++ resource/database.php
@@ -1,4 +1,19 @@
 <?php
+$dsn = 'mysql:host=localhost; dbname=register ';
+$username = 'root';
+$password = 'cinfotech';
+
+try {
+    $db = new PDO ($dsn, $username, $password);
+
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (\Throwable $th) {
+    //throw $th;
+}
+
+
+
+
 
 
     
